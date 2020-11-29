00828f6488ac91a4e3757df8a78526b4c14479f6
chrnndz3 <chrnndz3@gmail.com>
2020-11-29 09:45:28 -0600

    adding video chat

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import VideoChat from './VideoChat';
+
+const App = () => {
+  return (
+    <div className="app">
+      <header>
+        <h1></h1>
+      </header>
+      <main>
+        <VideoChat />
+      </main>
+      <footer>
+        <p>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
